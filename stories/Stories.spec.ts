65d3aa817809f99ddf70f07b5cea7d11313f1146
Roberto Ríos <roberto.rios.perea@outlook.com>
2020-08-28 17:52:46 +0200

    chore: Test

--- stories/Stories.spec.ts
+++ stories/Stories.spec.ts
@@ -1,4 +1,3 @@
 import initStoryshots from '@storybook/addon-storyshots'
-import { puppeteerTest } from '@storybook/addon-storyshots-puppeteer'
 
-initStoryshots({ suite: 'Puppeter storyshots', test: puppeteerTest() })
+initStoryshots()
